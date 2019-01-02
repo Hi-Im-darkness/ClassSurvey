360fc06d2886c0c91275d108d00b1ca5131f7ca5
Hi-Im-darkness <nghthach98@gmail.com>
2019-01-02 13:07:30 +0700

    add dosurvey

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -8,6 +8,7 @@ use App\Models\Course;
 use App\Models\Survey;
 use App\Models\Form;
 use App\Models\Question;
+use App\Models\Dosurvey;
 
 class SurveyController extends Controller
 {
@@ -18,7 +19,7 @@ class SurveyController extends Controller
                 break;
         }
         if (! $user)
-            return response()->json(ResponseWrapper::wrap(false, 400, 'reason', 'token invalid'), 400);
+            return response()->json(ResponseWrapper::wrap(false, 401, 'reason', 'token invalid'), 401);
 
         $courses = $user->courses()->with([
             'teacher' => function($query) { $query->select('id', 'name'); }
@@ -33,13 +34,14 @@ class SurveyController extends Controller
                 break;
         }
         if (! $user)
-            return response()->json(ResponseWrapper::wrap(false, 400, 'reason', 'token invalid'), 400);
+            return response()->json(ResponseWrapper::wrap(false, 401, 'reason', 'token invalid'), 401);
 
         if (! $user->hasPermission('do-survey'))
             return response()->json(ResponseWrapper::wrap(false, 401, 'reason', 'unauthorized'), 401);
 
 
         $courseid =  $request->get('courseid');
+        $id = Survey::where('course_id', $courseid)->value('id');
         $name = Survey::where('course_id', $courseid)->value('name');
         $form = Survey::where('course_id', $courseid)->first()->form()->first();
         $question = $form->questions(); 
@@ -51,9 +53,36 @@ class SurveyController extends Controller
             ]);
         }
         $data = [
+            'survey_id' => $id,
             'name' => $name,
             'form' => $data
         ];
         return response()->json(ResponseWrapper::wrap(true, 200, 'data', $data));
+    }
+
+    public function doSurvey(Request $request) {
+        foreach (['admin', 'student', 'teacher'] as $guard) {
+            $user = $request->user($guard);
+            if ($user)
+                break;
+        }
+        if (! $user)
+            return response()->json(ResponseWrapper::wrap(false, 400, 'reason', 'token invalid'), 400);
+
+        if (! $user->hasPermission('do-survey'))
+            return response()->json(ResponseWrapper::wrap(false, 401, 'reason', 'unauthorized'), 401);
+
+        $survey_id = $request->get('survey_id');
+        $ans_arr = $request->get('answers');
+        foreach ($ans_arr as $question_id => $ans) {
+            $dosurvey = new Dosurvey([
+                'student_id' => $user->value('id'),
+                'survey_id' => $survey_id,
+                'question_id' => $question_id,
+                'answer' => $ans
+            ]);
+            $dosurvey->save();
+        }
+        return response()->json(ResponseWrapper::wrap(true, 200, 'message', 'do survey successfully'));
     }
 }
