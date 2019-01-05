1b098d564efdca2ce73d8baf131966fc5469a89a
Hi-Im-darkness <nghthach98@gmail.com>
2019-01-05 21:32:05 +0700

    add route for import

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/* Route::get('/import/student', 'ExcelImportController@index'); */
-/* Route::post('/import/student', 'ExcelImportController@studentImport')->name('import'); */
-/* Route::get('/import/teacher', 'ExcelImportController@index'); */
-/* Route::post('/import/teacher', 'ExcelImportController@teacherImport')->name('import'); */
+Route::get('/import/student', 'ExcelImportController@index');
+Route::post('/import/student', 'ExcelImportController@studentImport');
+Route::get('/import/teacher', 'ExcelImportController@index');
+Route::post('/import/teacher', 'ExcelImportController@teacherImport');
 Route::get('/import/course', 'ExcelImportController@index');
-Route::post('/import/course', 'CourseController@import')->name('import');
+Route::post('/import/course', 'CourseController@import');
 
 Route::get('/login', 'AuthController@showLogin');
 Route::post('/login', 'AuthController@login');
@@ -33,9 +33,7 @@ Route::get('/courses/surveyform', 'SurveyController@showSurveyForm');
 Route::post('/courses/surveyform', 'SurveyController@doSurvey');
 
 Route::get('/surveys', 'SurveyController@showSurvey');
-/* Route::get('/surveys/add', 'SurveyController@gshowAddSurvey'); */
 Route::post('/surveys', 'SurveyController@addSurvey');
-/* Route::get('/surveys/edit', 'SurveyController@showEditSurvey'); */
 Route::put('/surveys', 'SurveyController@editSurvey');
 Route::delete('/surveys', 'SurveyController@deleteSurvey');
 Route::get('/surveys/result', 'SurveyController@showResult');
